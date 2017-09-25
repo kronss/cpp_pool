19b8af397476eac1275764831ac346b61f24d28f
Oleg Chaychenko <oleg.chaychenko@gmail.com>
2017-09-25 13:14:48 +0300

    ex02 start

--- day02/ex01/test.c
+++ day02/ex01/test.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 
+
+
+
+
+
+
+
+
+
+void print(int a, int b)
+{
+	printf("%d %d\n", a, b);
+}
+
+
+
+
+
+struct dick
+{
+	int a;
+	int b;
+	print(a, b);
+}bb;
+
+
+
+
+
 int main()
 {
+	// struct b;
+
+	bb.print;
+
 	static int a  = 5;
 	printf("%d\n", a--);
-	if (a)
-		main();
 	return 0;
 }
