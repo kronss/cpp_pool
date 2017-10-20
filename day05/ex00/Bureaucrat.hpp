4298dd6d9b9a5de146431e4028ece25cf9f2304f
Oleg Chaychenko <oleg.chaychenko@gmail.com>
2017-10-20 13:27:52 +0300

    tmp

---
+++ day05/ex00/Bureaucrat.hpp
@@ -0,0 +1,20 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+	class Bureaucrat
+	{
+	public:
+		Bureaucrat();
+		~Bureaucrat();
+		
+
+
+
+
+	private:
+		std::string _name;
+		int         _grade;
+	};
+
+
+#endif
